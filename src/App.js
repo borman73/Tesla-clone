a7445e47cc7d88374b548e76775be080a8ccd703
borman73 <zk-borman@yandex.ru>
2022-02-19 22:21:19 +0400

    Save progress 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div>
+      {/* <Header /> */}
+      <Home />
     </div>
   );
 }
