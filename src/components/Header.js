ba1f2f36de5b1c03e85f2dce7d094ff72fd514c4
borman73 <zk-borman@yandex.ru>
2022-02-21 21:32:28 +0400

    Save progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Header() {
   return (
@@ -16,6 +17,18 @@ function Header() {
         <a href='#'>Tesla Account</a>
         <CustomMenu></CustomMenu>
       </RightMenu>
+      <BurgerNav>
+        <CloseWrapper>
+          <CustomClose />
+        </CloseWrapper>
+        <li><a href='#'>Existing Inventory</a></li>
+        <li><a href='#'>Used Inventory</a></li>
+        <li><a href='#'>Trade-in</a></li>
+        <li><a href='#'>Cybertruck</a></li>
+        <li><a href='#'>Roadster</a></li>
+        <li><a href='#'>Semi</a></li>
+        <li><a href='#'>Test Drive</a></li>
+      </BurgerNav>
     </Container>
   )
 }
@@ -23,6 +36,7 @@ function Header() {
 export default Header;
 
 const Container = styled.div`
+  z-index: 1;
   min-height: 60px;
   position: fixed;
   display: flex;
@@ -53,6 +67,7 @@ const Menu = styled.div`
 const RightMenu = styled.div`
   display: flex;
   align-items: center;
+
   a {
     font-weight: 600;
     text-transform: uppercase;
@@ -62,3 +77,37 @@ const RightMenu = styled.div`
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
 `
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 100;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: 600;
+    }
+  }
+`
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`
+
